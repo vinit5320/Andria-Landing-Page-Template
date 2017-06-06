1cd64c7214a5f60e36dbed602dcd3a947de12e42
vinit5320 <vinit5320@gmail.com>
2017-06-05 23:54:01 -0400

    complete interface section, add sliding animation, fix section names

--- js/main.js
+++ js/main.js
@@ -21,4 +21,16 @@
         });
     }
 
+    $(window).scroll(function() {
+        var top_of_element = $(".img-wrap").offset().top;
+        var bottom_of_element = $(".img-wrap").offset().top + $(".img-wrap").outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).height();
+        var top_of_screen = $(window).scrollTop();
+        if((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+            $(".img-wrap").addClass("animate-img");
+            $(".text-wrap").addClass("animate-text-wrap");
+        }
+    });
+
+
 })();
